d4ffed0a32ff83fd9528f70fb8cc7cfbbed3a78e
Uladzimir Baranouski <uladbar@gmail.com>
2023-08-04 00:04:33 +0200

    All are saved

---
+++ Task41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+// Считывание числа с консоли
+int Prompt(string message)
+{
+    System.Console.WriteLine(message); // вывод сообщения
+    string? value = Console.ReadLine(); // считывание строки с консоли
+    int result = Convert.ToInt32(value); // преобразование строки в целое число
+    return result; // возврат результата
+}
+
+// Ввод массива
+int[] InputArray(int length)
+{
+    int [] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt ($"Введите {i + 1}-й элемент");
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count++;
+    }
+    return count;
+}
+
+int Length = Prompt("Введите количество элементов >");
+int[] array;
+array = InputArray(Length);
+PrintArray(array);
+Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
---
+++ Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+class Program
+{
+    static void Main()
+    {
+        double b1, k1, b2, k2;
+
+        // Вводим значения от пользователя
+        Console.Write("Введите значение b1: ");
+        b1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k1: ");
+        k1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение b2: ");
+        b2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение k2: ");
+        k2 = double.Parse(Console.ReadLine());
+
+        // Находим точку пересечения
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+
+        // Выводим результат
+        Console.WriteLine("Точка пересечения прямых: ({0}; {1})", x, y);
+    }
+}
